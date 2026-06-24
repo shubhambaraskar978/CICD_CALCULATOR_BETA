ca0acf2e18fda44a87ea3ad35e0c6e34b479802f
shubham baraskar <shubhambaraskar882@gmail.com>
2026-06-24 17:45:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## This project is belongs to CICD
---
+++ tests/test_calculator.py
@@ -0,0 +1,17 @@
+from app.calculator import add, subtract, multiply, divide
+
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+
+
+def test_multiply():
+    assert multiply(2, 4) == 8
+
+
+def test_divide():
+    assert divide(10, 2) == 5
